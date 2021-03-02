6e87e72a9e6e05bd59fbc3984fe1fb9a26180424
Tabatha Bell <tabathambell@live.com>
2021-03-02 17:22:27 -0500

    did user routes and index files

--- routes/api/user-routes.js
+++ routes/api/user-routes.js
@@ -1,16 +1,23 @@
 const router = require('express').Router();
+const {
+    getAllUsers,
+    getUserById,
+    createUser,
+    updateUser,
+    deleteUser
+} = require('../../controllers/user-controller');
 
 // Set up GET all and POST at /api/users
 router
   .route('/')
-  .get()
-  .post();
+  .get(getAllUsers)
+  .post(createUser);
 
 // Set up GET one, PUT, and DELETE at /api/users/:id
 router
   .route('/:id')
-  .get()
-  .put()
-  .delete();
+  .get(getUserById)
+  .put(updateUser)
+  .delete(deleteUser);
 
 module.exports = router;
