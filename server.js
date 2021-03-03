77a613f8fb3efb51b0c47281c8a897046c79c30f
Tabatha Bell <tabathambell@live.com>
2021-03-03 15:08:43 -0500

    finished routes

--- server.js
+++ server.js
@@ -8,8 +8,6 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static('public'));
 
-app.use(require('./routes'));
-
 mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/socialnetworkmongo', {
   useFindAndModify: false,
   useNewUrlParser: true,
@@ -17,4 +15,7 @@ mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/socialnetworkmo
 });
 
 mongoose.set('debug', true);
+
+app.use(require('./routes'));
+
 app.listen(PORT, () => console.log(`🌍 Connected on localhost:${PORT}`));
